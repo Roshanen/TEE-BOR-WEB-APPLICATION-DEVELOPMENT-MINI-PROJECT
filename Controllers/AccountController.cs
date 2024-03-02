3b808ea94de4484d98773ae0e5d320a018b7eec5
unknown <nichakarnink@gmail.com>
2024-03-02 19:10:30 +0700

    login & sign up

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+// using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+       
+    }
+}
