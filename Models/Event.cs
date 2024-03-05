cb9f6480258152a51bae2629c3d79faec5081833
best <kittibest46@gmail.com>
2024-03-06 00:52:59 +0700

    add more model and fix event model

--- Models/Event.cs
+++ Models/Event.cs
@@ -6,18 +6,16 @@ public class Event
 {
     public ObjectId Id { get; set; }
     public ObjectId PlaceId { get; set; }
+    public ObjectId TagId { get; set; }
+    public ObjectId HostId { get; set; }
+
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public DateTime LastModifiedDate { get; set; }
-    public string Name { get; set; }
-    public string Place { get; set; }
-    public string Description { get; set; }
-    public int MaxMember { get; set; }
-    public int CurrentMember { get; set; }
-    private IMongoCollection<Event> _collections;
-    
-    public Event(){
-        var context = new MongoContext();
-        _collections = context.GetCollection<Event>("events");
-    }
+
+    public string? EventName { get; set; }
+    public string? EventImg { get; set; }
+    public string? EventDetails { get; set; }
+    public int? CurrentMember { get; set; }
+    public int? MaxMember { get; set; }
 }
