2de92cf40e410a13ca8bbbb137f7a60cef81ebc6
best <kittibest46@gmail.com>
2024-03-06 01:53:13 +0700

    change place -> actulPlace

--- Models/Place.cs
+++ Models/Place.cs
@@ -7,7 +7,7 @@ public class Place
     public ObjectId Id { get; set; }
     
     public string? MapUrl { get; set; }
-    public string? Place { get; set; }
+    public string? ActualPlace { get; set; }
     public string? Province { get; set; }
     public string? District { get; set; }
     public string? SubDistrict { get; set; }
