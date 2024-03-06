f7fc9c1acd8e1a04389e024d382f586c00d091d7
best <kittibest46@gmail.com>
2024-03-06 11:03:13 +0700

    rename Img in user model to ProfilePicture

--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,6 @@ public class User
 {
     public ObjectId UserId { get; set; }
     public string? UserName { get; set; }
-    public string? Img { get; set; }
     public string? Email { get; set; }
+    public string? ProfilePicture { get; set; }
 }
