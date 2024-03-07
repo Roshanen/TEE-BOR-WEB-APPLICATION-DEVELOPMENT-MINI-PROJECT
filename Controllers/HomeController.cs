ecfde37effa2338b34c8899e782d5c70f4e45d48
Roshanen <numesattahip@hotmail.com>
2024-03-07 23:30:32 +0700

    feat: basic userlog-in display

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,8 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using System.Diagnostics;
 using WebApp.Models;
 
 namespace WebApp.Controllers;
@@ -9,32 +10,41 @@ namespace WebApp.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly MongoContext _mongoContext;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, MongoContext mongoContext)
     {
         _logger = logger;
+        _mongoContext = mongoContext;
     }
+
 
     public IActionResult Index()
     {
-        var jwtToken = HttpContext.Session.GetString("JwtToken");
-        var userId = JwtHelper.GetUserIdFromToken(jwtToken!);
+        var userId = JwtHelper.GetUserIdFromToken(HttpContext.Session.GetString("JwtToken")!);
+        ViewData["userID"] = userId;
+        if (userId != null)
+        {
+            var userName = _mongoContext.GetCollection<User>("users").Find(u => u.Id == ObjectId.Parse(userId)).FirstOrDefault();
+            ViewData["userName"] = userName.UserName;
+        }
 
-        var connectionUri = Environment.GetEnvironmentVariable("MONGODB_CONNECTION_STRING");
-        var settings = MongoClientSettings.FromConnectionString(connectionUri);
-        settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-        var client = new MongoClient(settings);
-        try
-        {
-            var result = client
-                .GetDatabase("admin")
-                .RunCommand<BsonDocument>(new BsonDocument("ping", 1));
-            Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex);
-        }
+        // var connectionUri = Environment.GetEnvironmentVariable("MONGODB_CONNECTION_STRING");
+        // var settings = MongoClientSettings.FromConnectionString(connectionUri);
+        // settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+        // var client = new MongoClient(settings);
+        // try
+        // {
+        //     var result = client
+        //         .GetDatabase("admin")
+        //         .RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+        //     Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+        // }
+        // catch (Exception ex)
+        // {
+        //     Console.WriteLine(ex);
+        // }
+
         return View();
     }
 
