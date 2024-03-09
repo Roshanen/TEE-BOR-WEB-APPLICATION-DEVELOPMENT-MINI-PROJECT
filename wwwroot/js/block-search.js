6a79fed25927e38883281c835ddaa59f56839f68
Roshanen <numesattahip@hotmail.com>
2024-03-09 23:25:49 +0700

    feat: add search using ajax

--- wwwroot/js/block-search.js
+++ wwwroot/js/block-search.js
@@ -6,3 +6,36 @@ const inputBaseBlock = baseBlock.childNodes[1].childNodes[3];
 inputBaseBlock.addEventListener("focus", () => {
   extendBlock.style.display = "flex";
 });
+
+function search() {
+  var name = $("#name").val() || "";
+  var place = $("#place").val() || "";
+  var dateChoice = $("#dateChoice").val() || "any";
+  var type = $("#type").val() || "any";
+  var category = $("#category").val() || "any";
+
+  var inputValues = {
+      name: name,
+      place: place,
+      dateChoice: dateChoice,
+      type: type,
+      category: category
+  };
+
+  $.ajax({
+      type: "POST",
+      url: "/search",
+      data: inputValues,
+      success: function (data) {
+          $("#searchResultsContainer").html(data);
+          $("#name").val(inputValues.name);
+          $("#place").val(inputValues.place);
+          $("#dateChoice").val(inputValues.dateChoice);
+          $("#type").val(inputValues.type);
+          $("#category").val(inputValues.category);
+      },
+      error: function (error) {
+          console.error(error);
+      }
+  });
+}
