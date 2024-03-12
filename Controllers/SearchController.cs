940de2f7dc4f4337bae9c67cbeb80984d45afd2f
Victorykung5 <Victorykung5@gmail.com>
2024-03-12 15:05:27 +0700

    merge

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -20,77 +20,7 @@ public class SearchController : BaseController
         _SetUserDataInViewData();
         try
         {
-            var eventsCollection = _mongoContext.GetCollection<Event>("events");
-            var filterBuilder = Builders<Event>.Filter;
-            var filter = filterBuilder.Empty;
-
-            if (!string.IsNullOrEmpty(search.Name))
-            {
-                filter &= filterBuilder.Regex(
-                    "EventName",
-                    new BsonRegularExpression(search.Name, "i")
-                );
-            }
-
-            if (!string.IsNullOrEmpty(search.Place))
-            {
-                var placesCollection = _mongoContext.GetCollection<Place>("places");
-                var place = placesCollection
-                    .Find(p => p.ActualPlace == search.Place)
-                    .FirstOrDefault();
-
-                if (place != null)
-                {
-                    filter &= filterBuilder.Eq("PlaceId", place.Id);
-                }
-                else
-                {
-                    return View("events", new List<Event>());
-                }
-            }
-
-            if (!string.IsNullOrEmpty(search.DateChoice) && search.DateChoice != "any")
-            {
-                DateTime currentDate = DateTime.UtcNow.Date;
-                switch (search.DateChoice.ToLower())
-                {
-                    case "today":
-                        filter &=
-                            filterBuilder.Gte("EndDate", currentDate)
-                            & filterBuilder.Lt("EndDate", currentDate.AddDays(1));
-                        break;
-                    case "this week":
-                        filter &=
-                            filterBuilder.Gte("EndDate", currentDate)
-                            & filterBuilder.Lt("EndDate", currentDate.AddDays(7));
-                        break;
-                    case "next week":
-                        filter &=
-                            filterBuilder.Gte("EndDate", currentDate.AddDays(7))
-                            & filterBuilder.Lt("EndDate", currentDate.AddDays(14));
-                        break;
-                }
-            }
-
-            if (!string.IsNullOrEmpty(search.Type) && search.Type != "any")
-            {
-                filter &= filterBuilder.Eq("EventType", search.Type);
-            }
-
-            if (!string.IsNullOrEmpty(search.Category) && search.Category != "any")
-            {
-                var tag = _mongoContext
-                    .GetCollection<Category>("tags")
-                    .Find(t => t.CategoryName == search.Category)
-                    .FirstOrDefault();
-
-                if (tag != null)
-                {
-                    filter &= filterBuilder.Eq("TagId", tag.Id);
-                }
-            }
-
-            var events = eventsCollection.Find(filter).ToList();
+            List<Event> events = searchFilters(search);
 
             return View(events);
         }
@@ -99,4 +29,102 @@ public class SearchController : BaseController
             return StatusCode(500, $"Internal server error: {ex.Message}");
         }
     }
+
+    public IActionResult ViewResult(Search search)
+    {
+        _SetUserDataInViewData();
+        try
+        {
+            List<Event> events = searchFilters(search);
+
+            return View(events);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+
+    public List<Event> searchFilters(Search search)
+    {
+        var filterBuilder = Builders<Event>.Filter;
+        var filter = filterBuilder.Empty;
+
+        DateTime currentDate = DateTime.UtcNow.Date;
+
+        if (!string.IsNullOrEmpty(search.Name))
+        {
+            filter &= filterBuilder.Regex("EventName",new BsonRegularExpression(search.Name, "i"));
+        }
+
+        if (!string.IsNullOrEmpty(search.Status) && search.Status != "any")
+        {
+            if (search.Status == "Available")
+            {
+                filter &= filterBuilder.Eq("Status", "Active")
+                    & filterBuilder.Gte("EndDate", currentDate);
+            }
+            if (search.Status == "Past")
+            {
+                filter &= filterBuilder.Lt("EndDate", currentDate);
+            }
+            if (search.Status == "Cancelled")
+            {
+                filter &= filterBuilder.Eq("Status", "Cancelled");
+            }
+            if (search.Status == "Close")
+            {
+                filter &= filterBuilder.Eq("Status", "Close");
+            }
+        }
+
+        if (!string.IsNullOrEmpty(search.DateChoice) && search.DateChoice != "any")
+        {
+            switch (search.DateChoice.ToLower())
+            {
+                case "today":
+                    filter &=
+                        filterBuilder.Gte("EndDate", currentDate)
+                        & filterBuilder.Lt("EndDate", currentDate.AddDays(1));
+                    break;
+                case "this week":
+                    filter &=
+                        filterBuilder.Gte("EndDate", currentDate)
+                        & filterBuilder.Lt("EndDate", currentDate.AddDays(7));
+                    break;
+                case "next week":
+                    filter &=
+                        filterBuilder.Gte("EndDate", currentDate.AddDays(7))
+                        & filterBuilder.Lt("EndDate", currentDate.AddDays(14));
+                    break;
+            }
+        }
+
+            Console.WriteLine(search.Type); 
+        if (!string.IsNullOrEmpty(search.Type) && search.Type != "any")
+        {
+            filter &= filterBuilder.Eq("Type", search.Type);
+        }
+
+        if (!string.IsNullOrEmpty(search.Category) && search.Category != "any")
+        {
+            filter &= filterBuilder.Eq("CategoryName", search.Category);
+        }
+
+        var events = _mongoContext.GetCollection<Event>("events").Find(filter).ToList();
+
+        if (!string.IsNullOrEmpty(search.Status) && search.Status != "any"){
+            if (search.Status == "Available")
+            {
+                events = events.Where(ev => ev.CurrentMember < ev.MaxMember).ToList();
+            }
+            if (search.Status == "Full")
+            {
+                events = events.Where(ev => ev.CurrentMember >= ev.MaxMember).ToList();
+            }
+        }
+
+        return events;
+    }
+
 }
